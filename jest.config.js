c1624e6b4712ca7f81513f31dc9a9a7354811006
BjornLuG <bjornlu.dev@gmail.com>
2019-08-06 22:54:40 +0800

    Config Jest again

--- jest.config.js
+++ jest.config.js
@@ -2,33 +2,25 @@ module.exports = {
   setupFiles: [
     '<rootDir>/__tests__/setup.js'
   ],
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
-    'vue'
-  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
   },
   transformIgnorePatterns: [
     '/node_modules/'
   ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1'
-  },
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ],
   testMatch: [
-    '**/__tests__/**/*.spec.(js|jsx|ts|tsx)'
+    '**/__tests__/**/*.spec.js'
   ],
-  testURL: 'http://localhost/',
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootdir>/components/**/*.vue',
+    '<rootdir>/pages/**/*.vue'
   ]
 }
